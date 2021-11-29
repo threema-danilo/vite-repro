8715ad93c175bb5b3a5d65a4c4d7ce1593e51b7f
Danilo Bargen <danilo.bargen@threema.ch>
2021-11-29 15:08:46 +0100

    Add direct node dependency

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import App from './App.svelte'
 
+import {promisify} from 'node:util';
+
+
 const app = new App({
   target: document.getElementById('app')
 })
+
+const promisified = promisify((callback) => callback());
 
 export default app
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import commonjsExternals from 'vite-plugin-commonjs-externals';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+    plugins: [
+        svelte(),
+        commonjsExternals.default({externals: [
+            'node:util',
+        ]}),
+    ]
 })
